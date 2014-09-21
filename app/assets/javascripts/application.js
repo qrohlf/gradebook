fbb023bb70e0365f20ee4f1750c47620f1cdf0bd
Quinn Rohlf <qr@qrohlf.com>
2014-09-21 11:46:24 -0700

    apparently I have to require jquery after the bootstrap js???

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
 //= require bootstrap
+//= require jquery
+//= require jquery_ujs
