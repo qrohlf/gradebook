7b13368f17acf4b9fc7c45d43087831f668db72e
Quinn Rohlf <qr@qrohlf.com>
2014-09-21 11:25:46 -0700

    trigger resync on assignment tag creation/change

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,12 @@
 class Assignment < ActiveRecord::Base
+  after_create :trigger_resync
+  after_update :trigger_resync, if: :tag_prefix_changed?
+
   def to_s
     title
+  end
+
+  def trigger_resync
+    Student.update_all(last_sync: nil)
   end
 end
